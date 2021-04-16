f930a90dcc3839c79988fa8c6ac2d5f27ad376dd
petarbratic <bratic.petar03@gmail.com>
2021-04-16 16:10:01 +0200

    ubacen restoran i skladiste, stabilizacija kamere

---
+++ Assets/Scripts/CameraStable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+    public GameObject TheCar;
+    public float CarY;
+
+    void Update()
+    {
+        CarY = TheCar.transform.eulerAngles.y;
+        transform.eulerAngles = new Vector3(0, CarY, 0);
+    }
+}
